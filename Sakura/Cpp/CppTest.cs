24746ecc41634cafb95562c88c1945dec9c9ca09
LunarEclipse <949886@qq.com>
2019-01-19 11:13:07 +0800

    C++ Interop & EventBus ThreadMode
    1. Introducing c++ Interop.
    2. EventBus thread mode in developing.

---
+++ Sakura/Cpp/CppTest.cs
@@ -0,0 +1,18 @@
+﻿//
+//  CppTest.cs
+//  Sakura
+//
+//  Created by LunarEclipse on 2019-01-19 10:07:25.
+//  Copyright © 2019 LunarEclipse. All rights reserved.
+//
+
+using System.Runtime.InteropServices;
+
+namespace Sakura.Cpp
+{
+    public class CppTest
+    {
+        [DllImport("Sakura.Cpp.dll")]
+        public static extern int Add(int n1, int n2);
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Sakura.Core.Event;
+using Sakura.Extensions;
+using Xunit;
+
+namespace SakuraTest
+{
+    public class UnitTest1
+    {
+        //        SakuraListener listener = new SakuraListener();
+
+        [Fact]
+        public void Test1()
+        {
+            Trace.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            var thread = new Thread(ThreadExec);
+            thread.Start();
+
+            var obj = new SakuraListener();
+            var obj2 = new SakuraListener2();
+            EventBus.Default.Post(new Object());
+            EventBus.Default.Post("");
+            EventBus.Default.Post(1);
+            EventBus.Default.Post(1.1);
+            EventBus.Default.Unregister(obj2);
+            EventBus.Default.Post(new Object());
+
+        }
+
+
+        [Fact]
+        public void Test2()
+        {
+            while (true)
+            {
+                var obj = new SakuraListener();
+                EventBus.Default.Post(1);
+                EventBus.Default.Unregister(obj);
+            }
+        }
+
+
+        [Fact]
+        public void Test3()
+        {
+            var a = Add(1, 2);
+            Trace.WriteLine("Hello World!" + a);
+        }
+
+
+        [DllImport("Cpp.dll")]
+        private static extern int Add(int n1, int n2);
+
+        private void ThreadExec()
+        {
+
+            Trace.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public class SakuraListener
+        {
+            public SakuraListener()
+            {
+                EventBus.Default.Register(this);
+            }
+
+            ~SakuraListener()
+            {
+                //                EventBus.Default.Unregister(this);
+                //                Trace.WriteLine("SakuraListener.Deinit");
+            }
+
+            public void Test(object args)
+            {
+                Trace.WriteLine("test");
+            }
+
+            [Subscribe]
+            public void Callback(int args)
+            {
+                Trace.WriteLine("test");
+            }
+        }
+
+        public class SakuraListener2
+        {
+            public SakuraListener2()
+            {
+                EventBus.Default.Register(this);
+            }
+
+            ~SakuraListener2()
+            {
+                //            EventBus.Default.Unregister(this);
+            }
+
+            [Subscribe]
+            public void Callback(object args)
+            {
+                Trace.WriteLine("test2");
+            }
+
+            [Subscribe]
+            public void Callback(string args)
+            {
+                Trace.WriteLine("test4");
+            }
+
+            [Subscribe]
+            public void Callback(int args)
+            {
+                Trace.WriteLine("test5");
+            }
+
+            [Subscribe]
+            public void Callback(double args)
+            {
+                Trace.WriteLine("test6");
+            }
+        }
+    }
+}
